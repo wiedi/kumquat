c7f4feb9a9470f2e9a334f02a9d12d8e5830724d
Sebastian Wiedenroth <wiedi@frubar.net>
2014-08-14 15:25:53 +0200

    change ftp account model from domain to vhost

--- ftp/forms.py
+++ ftp/forms.py
@@ -5,5 +5,5 @@ class AccountUpdateForm(forms.ModelForm):
 	new_password = forms.CharField(required=False, widget=forms.widgets.PasswordInput)
 	class Meta:
 		model = Account
-		fields = ('domain', 'path')
+		fields = ('vhost', 'path')
 
--- ftp/models.py
+++ ftp/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 from passlib.hash import sha512_crypt
-from kumquat.models import Domain
+from web.models import VHost
 
 default_length = 255
 
 class Account(models.Model):
 	name     = models.CharField(max_length=default_length, unique=True)
 	password = models.CharField(max_length=default_length)
-	domain   = models.ForeignKey(Domain, related_name='ftp_accounts')
+	vhost    = models.ForeignKey(VHost, null=True, blank=True)
 	path     = models.CharField(max_length=default_length, default="/")
 
 	def set_password(self, password):
