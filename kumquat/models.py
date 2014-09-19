07ca3c1703bd64fb6db194117997c8abdd9bff15
Thomas Merkel <tm@core.io>
2014-09-19 19:02:09 +0200

    Add help text to Domain and Subdomain / Vhost parts and rename "Name" to "Sub Domain"

--- kumquat/models.py
+++ kumquat/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 class Domain(models.Model):
-	name = models.CharField(max_length=255, unique=True, verbose_name=_('Name'))
+	name = models.CharField(max_length=255, unique=True, verbose_name=_('Name'), help_text=_('Your primary domain (example.com) that you like to use for the different services.'))
 
 	def __unicode__(self):
 		return self.name
