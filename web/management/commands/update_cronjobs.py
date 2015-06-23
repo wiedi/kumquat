ada6137d11ebac0a37d7595c16c66f8d2643ea73
Thomas Merkel <tm@core.io>
2015-06-23 09:44:41 +0200

    Use subprocess pipe to run cron commands

--- web/management/commands/update_cronjobs.py
+++ web/management/commands/update_cronjobs.py
@@ -1,15 +1,17 @@
 from django.core.management.base import BaseCommand, CommandError
-from django.db.models import Count
 from django.template.loader import render_to_string
 from django.conf import settings
 from cron.models import Cronjob
-from subprocess import call
-import uuid
-import os
+import subprocess
 
 def update_cronjobs():
+    cronlist = ''
+    cron = subprocess.Popen(settings.KUMQUAT_CRONJOB_CMD, stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     for cronjob in Cronjob.objects.all():
-        print(cronjob.when + " " + cronjob.command)
+        cronlist += cronjob.when + " " + cronjob.command + "\n"
+
+    cron.stdin.write(cronlist)
+    cron.stdin.close()
 
 class Command(BaseCommand):
     args = ''
