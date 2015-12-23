cd2394493cb823f3b27a344f7d11662a7968a4e0
Sebastian Wiedenroth <wiedi@frubar.net>
2015-12-23 13:19:49 +0100

    fix delete_vhosts to find correct zfs dataset

--- kumquat/management/commands/delete_vhosts.py
+++ kumquat/management/commands/delete_vhosts.py
@@ -12,7 +12,7 @@ class Command(BaseCommand):
 	def handle(self, *args, **options):
 		if settings.KUMQUAT_USE_ZFS:
 			for vhost in os.listdir(settings.KUMQUAT_VHOST_ROOT + '/.Trash/'):
-				call(['zfs', 'destroy', '-r', vhost])
+				call(['zfs', 'destroy', '-r', settings.KUMQUAT_VHOST_DATASET + '/.Trash/' + vhost])
 		else:
 			trash = settings.KUMQUAT_VHOST_ROOT + '/.Trash/'
 			shutil.rmtree(trash)
