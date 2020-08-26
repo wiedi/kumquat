6616b1158428f9c288914330e0386f115edf239b
Thomas Merkel <tm@core.io>
2020-08-26 12:29:23 +0200

    Add Let's Encrypt Status Overview
    Add a simple LE status overview to show the current issues of
    certificate requests.

--- status/views.py
+++ status/views.py
@@ -1,7 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from status.info import info
+from web.models import LetsEncrypt, SSLCert
 
 @login_required
 def status(request):
-	return render(request, 'status/status.html', {'info': info()})
+	le_info = LetsEncrypt.objects.exclude(last_message__isnull=True).exclude(last_message__exact='')
+	return render(request, 'status/status.html', {'info': info(), 'le_info': le_info})
